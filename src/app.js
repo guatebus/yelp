74016f7c9136169b621dda7b974fd83cb908cb17
Alejandro Bustamante <alejandro.bustamante@scmspain.com>
2016-05-31 18:31:10 +0200

    initial commit. Hot loading and global/module css styles working

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import styles from './styles.module.css';
+
+import './app.css';
+
+const App = React.createClass({
+  render: function() {
+    return (<div className={styles.wrapper}>
+      Yoooooe!</div>
+    );
+  }
+});
+
+const mountNode = document.querySelector('#root');
+ReactDOM.render(<App />, mountNode);
